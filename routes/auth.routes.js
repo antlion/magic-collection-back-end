dd8718e63913c690b9216d1f30e71d4620c2fd9a
Lorenzo Napoleoni <lorenzo.napoleoni@tholosphp.com>
2020-05-04 18:02:21 +0200

    update

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -163,39 +163,6 @@ router.post("/my-decks/:id/add",(req, res, next) => {
     });
 })
 
-router.post("/my-decks/:id/patch",(req, res, next) => {
-
-    const filter = { _id: req.body.id };
-    const update = {
-        creatures: req.body.creatures,
-        artifacts: req.body.artifacts,
-        enchantments: req.body.enchantments,
-        planeswalkers: req.body.planeswalkers,
-        spells: req.body.spells,
-        lands: req.body.lands,
-        sideboard: req.body.sideboard
-    };
-    let doc = deckSchema.findByIdAndUpdate(req.body.id, update, function(err, result) {
-        if (err) {
-            res.send(err);
-        } else {
-            res.send(result);
-        }
-    })
-})
-
-
-router.route('/my-decks/:id/:id_deck').get(authorize, (req, res, next) => {
-    deckSchema.findById(req.params.id_deck, (error, data) => {
-        if (error) {
-            return next(error);
-        } else {
-            res.status(200).json({
-                data
-            })
-        }
-    })
-})
 
 // Delete User
 router.route('/delete-user/:id').delete((req, res, next) => {
--- routes/decks.routes.js
+++ routes/decks.routes.js
@@ -16,4 +16,74 @@ router.route("/my-decks/:id_user/:id_deck/delete").get(authorize,(req, res, next
     })
 })
 
+
+router.post("/my-decks/:id/patch",(req, res, next) => {
+
+    const filter = { _id: req.body.id };
+    const update = {
+        creatures: req.body.creatures,
+        artifacts: req.body.artifacts,
+        enchantments: req.body.enchantments,
+        planeswalkers: req.body.planeswalkers,
+        spells: req.body.spells,
+        lands: req.body.lands,
+        sideboard: req.body.sideboard,
+        mayboard: req.body.mayboard
+    };
+    let doc = deckSchema.findByIdAndUpdate(req.body.id, update, function(err, result) {
+        if (err) {
+            res.send(err);
+        } else {
+            res.send(result);
+        }
+    })
+})
+
+
+router.route('/my-decks/:id/:id_deck').get(authorize, (req, res, next) => {
+    deckSchema.findById(req.params.id_deck, (error, data) => {
+        if (error) {
+            return next(error);
+        } else {
+            res.status(200).json({
+                data
+            })
+        }
+    })
+})
+
+
+// Get Single User
+router.route('/my-decks/:id').get(authorize, (req, res, next) => {
+    deckSchema.find({userId: req.params.id} , (error, data) => {
+        if (error) {
+            return next(error);
+        } else {
+            res.status(200).json({
+                data
+            })
+        }
+    })
+})
+
+router.post("/my-decks/:id/add",(req, res, next) => {
+    const deck = new deckSchema({
+        name: req.body.name,
+        type: req.body.type,
+        userId: req.params.id
+    });
+    deck.save().then((response) => {
+        res.status(201).json({
+            message: "Deck Added!",
+            result: response
+        });
+    }).catch(error => {
+        res.status(500).json({
+            error: error
+        });
+    });
+})
+
+
+
 module.exports = router;
